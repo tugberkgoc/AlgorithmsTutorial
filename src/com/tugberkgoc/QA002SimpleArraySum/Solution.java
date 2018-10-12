ffd9ba6789e55539fd33f3233054c8459ebf207f
tugberkgoc <tugberk.goc@bilgiedu.net>
2018-10-12 09:25:07 +0300

    first commit

---
+++ src/com/tugberkgoc/QA002SimpleArraySum/Solution.java
@@ -0,0 +1,50 @@
+package com.tugberkgoc.QA002SimpleArraySum;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Solution {
+
+	/*
+	 * Complete the simpleArraySum function below.
+	 */
+	static int simpleArraySum(int[] ar) {
+		int temp = 0;
+		for(int i=0; i < ar.length; i++) {
+			temp += ar[i];
+		}
+		return temp;
+	}
+
+	// private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("src/com/tugberkgoc/QA002SimpleArraySum/output/output00.txt")); // System.getenv("OUTPUT_PATH")
+
+		final Scanner scanner = new Scanner(new File("src/com/tugberkgoc/QA002SimpleArraySum/input/input00.txt"));
+
+		int arCount = Integer.parseInt(scanner.nextLine().trim());
+
+		int[] ar = new int[arCount];
+
+		String[] arItems = scanner.nextLine().split(" ");
+
+		for (int arItr = 0; arItr < arCount; arItr++) {
+			int arItem = Integer.parseInt(arItems[arItr].trim());
+			ar[arItr] = arItem;
+		}
+
+		int result = simpleArraySum(ar);
+
+		bufferedWriter.write(String.valueOf(result));
+		bufferedWriter.newLine();
+
+		bufferedWriter.close();
+
+	}
+
+}
